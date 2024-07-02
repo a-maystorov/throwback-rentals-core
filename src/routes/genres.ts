0d672bb599f351454aa0e1fdbf6179d9cf38cac8
a-maystorov <alkin971@gmail.com>
2024-07-02 21:36:35 +0200

    add: genres GET /:id with validation and tests

--- src/routes/genres.ts
+++ src/routes/genres.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import validateObjectId from "../middleware/validateObjectId";
 import { Genre } from "../models/genre";
 
 const router = Router();
@@ -6,6 +7,16 @@ const router = Router();
 router.get("/", async (_, res) => {
   const genres = await Genre.find().sort({ name: 1 });
   res.send(genres);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+
+  res.send(genre);
 });
 
 export default router;
--- tests/routes/genres.test.ts
+++ tests/routes/genres.test.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { afterEach, describe, expect, it } from "vitest";
 import { Genre, IGenre } from "../../src/models/genre";
@@ -5,7 +6,7 @@ import { server } from "../../src/server";
 
 describe("/api/genres", () => {
   afterEach(async () => {
-    await server.close();
+    server.close();
     await Genre.deleteMany({});
   });
 
@@ -22,6 +23,32 @@ describe("/api/genres", () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some((g: IGenre) => g.name === "genre1")).toBeTruthy();
       expect(res.body.some((g: IGenre) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/api/genres/1");
+
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404 if no genre with the given id exists", async () => {
+      const id = new mongoose.Types.ObjectId();
+
+      const res = await request(server).get("/api/genres/" + id);
+
+      expect(res.status).toBe(404);
     });
   });
 });
